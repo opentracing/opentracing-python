c6a1f26c3017cd9a9619c2cc17014709a0e47471
Yuri Shkuro <ys@uber.com>
2015-12-29 17:17:08 -0500

    Back to development: 0.3.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='opentracing',
-    version='0.3.0',
+    version='0.3.1.dev0',
     author='Yuri Shkuro',
     author_email='ys@uber.com',
     description='Opentracing API',
