08f3a11b5e85f21442babf6d47f9949f1b48c892
Yuri Shkuro <ys@uber.com>
2016-08-03 12:04:47 -0400

    Back to development: 1.0rc6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='opentracing',
-    version='1.0rc5',
+    version='1.0rc6.dev0',
     author='Yuri Shkuro, The OpenTracing Authors',
     author_email='shkuro@gmail.com',
     description='OpenTracing API for Python. See documentation at http://opentracing.io',
