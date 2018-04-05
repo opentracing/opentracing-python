5cdbc94f6d2c20f28e4983abb01031726f5a8e8c
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2018-04-05 17:21:20 +0200

    Remove TEST_NAMES from testbed/__init__.py
    We don't list the actual test list anymore,
    as we simply load any of them that starts with the test_
    prefix.

--- testbed/__init__.py
+++ testbed/__init__.py
@@ -1,10 +1,1 @@
-TEST_NAMES = [
-    'test_active_span_replacement',
-    'test_client_server',
-    'test_common_request_handler',
-    'test_late_span_finish',
-    'test_listener_per_request',
-    'test_multiple_callbacks',
-    'test_nested_callbacks',
-    'test_subtask_span_propagation',
-]
+
